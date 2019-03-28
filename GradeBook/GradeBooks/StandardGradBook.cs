fbd59e9149fedec29830f2c0fb443bfc0a17d3b7
Becky <becky_austin@hotmail.com>
2019-03-28 16:10:30 -0400

    Completed the Module
    Completed the Module

---
+++ GradeBook/GradeBooks/StandardGradBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradBook: BaseGradeBook
+    {
+        public StandardGradBook(string name): base(name)
+        {
+            this.Type = GradeBookType.Standard;
+        }
+    }
+}
